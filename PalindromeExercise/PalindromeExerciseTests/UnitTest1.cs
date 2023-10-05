0889daecc3f643dc0539a399d5a0802c0a21f168
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-10-05 13:31:41 -0400

    Palindrome Exercise + Tests Complete!

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("wow", true)]
+        [InlineData("TrueCoders", false)]
+        [InlineData("Palindrome", false)]
+        public void MyTest(string word, bool expected)
         {
+            var test = new WordSmith();
 
+            bool actual = test.isAPalinDrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
